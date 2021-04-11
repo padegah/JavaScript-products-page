690068ebbf6b6c467d57aad254c9b30b8c5070ab
prince adegah <adegah.prince@yahoo.com>
2021-04-11 23:18:57 +0000

    completed nav bar design

---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+let product = [
+    {
+        title: "Conditioner",
+        price: 14.99,
+        stock: 10,
+        description: "Wonder Gro Jamaican Black Castor Oil Hair and Scalp Conditioner",
+        imgUrl: "/assets/images/conditioner.jpg"
+    },
+    {
+        title: "Hair Loss",
+        price: 14.99,
+        stock: 10,
+        description: "Genuine African Formula Shea Butter",
+        imgUrl: "/assets/images/loss.jpg"
+    },
+    {
+        title: "Relaxer",
+        price: 14.99,
+        stock: 10,
+        description: "Aclo Affirm Sensitive Scalp Relaxer",
+        imgUrl: "/assets/images/relaxer.jpg"
+    },
+    {
+        title: "Shampoo",
+        price: 14.99,
+        stock: 10,
+        description: "ApHogee Deep Moisture Shampoo",
+        imgUrl: "/assets/images/shampoo.jpg"
+    }
+]
